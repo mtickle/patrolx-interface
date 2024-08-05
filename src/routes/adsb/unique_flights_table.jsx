b71e51aa1d010485d1833be88637d8528d573a4c
Mike Tickle <mtickle@gmail.com>
2024-08-05 14:14:54 -0400

    Went back to local API

---
+++ src/routes/adsb/unique_flights_table.jsx
@@ -0,0 +1,74 @@
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+//--- STANDARD IMPORTS: DATA
+import { DataEndpoint } from '../../components/data/adsb_data_endpoint';
+
+//-- STANDARD IMPORTS: TABLE
+import DataTable from '../../components/layout/data_table';
+
+//--- BUILD TABLE
+function PageTable({ columns, data }) {
+    return DataTable(columns, data)
+}
+
+//--- BUILD TABLE COLUMNS
+function TableColumns() {
+
+    const columns = React.useMemo(
+        () => [
+           
+            {
+                Header: "Flight",
+                accessor: "Flight",
+            },
+            {
+                Header: "Operator",
+                accessor: "operator",
+            },
+            {
+                Header: 'ICAO24',
+                accessor: 'HexCode',
+            },
+            {
+                Header: 'Model',
+                accessor: 'Model',
+            },
+            {
+                Header: 'Cat',
+                accessor: 'category',
+            },
+            {
+                Header: 'Manufacturer',
+                accessor: 'manufacturerName',
+            },
+            {
+                Header: 'Registration',
+                accessor: 'registration',
+            }
+            ,
+            {
+                Header: 'Count',
+                accessor: 'FlightCount',
+            }
+        ],
+        []
+    )
+    return columns
+}
+
+export const UniqueFlightsDataTable = () => {
+
+    //--- LOAD DATA
+    var getAllName = "getAllUniqueFlights"
+    var tableColumns = TableColumns();
+    var tableData = DataEndpoint(getAllName, 100);
+
+    return (
+        <>
+            <h5>Data Table</h5>
+            <PageTable columns={tableColumns} data={tableData} />
+        </>
+    );
+};
