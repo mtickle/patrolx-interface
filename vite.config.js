d1443a752e6a080aff046a1762078ef175aac58b
Mike Tickle <mtickle@gmail.com>
2024-09-02 08:17:01 -0400

    Never did get the vite config chnages made, so here they are.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+// okay this is a trick
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/patrolx-interface/',
