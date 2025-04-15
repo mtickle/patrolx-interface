b78dc12b35731ee0a17676cb7f889bc88c21b60a
Mike Tickle <mtickle@gmail.com>
2025-04-15 09:20:32 -0400

    Working on the API

--- src/components/data/data_endpoint.jsx
+++ src/components/data/data_endpoint.jsx
@@ -13,8 +13,8 @@ export const DataEndpoint = (apiEndPoint, itemCount) => {
     };
 
     const client = axios.create({
-                baseURL: "http://192.168.86.2:8080/api/" + apiEndPoint
-                //baseURL: "http://localhost:8080/api/" + apiEndPoint
+                //baseURL: "http://192.168.86.2:8080/api/" + apiEndPoint
+                baseURL: "http://localhost:8080/api/" + apiEndPoint
     });
 
     console.log("DataEndpoint: " + apiEndPoint + ":" + itemCount)
