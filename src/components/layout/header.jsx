84ccecdadded72153f67064981eddd83dc31ed27
Mike Tickle <mtickle@gmail.com>
2025-05-13 11:28:16 -0400

    Still trying to fix these links

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -10,13 +10,11 @@ export default function Header() {
                 <Navbar.Brand href="/">Home</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        {/* <Nav.Link href="/Adsb">ADSB</Nav.Link>
-                        <Nav.Link href="/Arrests">Arrests</Nav.Link> */}
-                        <Nav.Link href="/Calls">Calls</Nav.Link>
-                        <Nav.Link href="/Incidents">Incidents</Nav.Link>
-                        <Nav.Link href="/TrafficStops">Traffic Stops</Nav.Link>
-                        <Nav.Link href="/CrashLocations">Crash Locations</Nav.Link>
+                    <Nav className="me-auto">                       
+                        <Nav.Link href="/patrolx-interface/Calls">Calls</Nav.Link>
+                        <Nav.Link href="/patrolx-interface/Incidents">Incidents</Nav.Link>
+                        <Nav.Link href="/patrolx-interface/TrafficStops">Traffic Stops</Nav.Link>
+                        <Nav.Link href="/patrolx-interface/CrashLocations">Crash Locations</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
                 
